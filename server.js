9ce20045b86e00584c68f3e08c5c7b8dbfe83eae
Malcolm Lynch <milynch1320@gmail.com>
2019-06-20 16:42:06 -0500

    adding get

--- server.js
+++ server.js
@@ -3,5 +3,33 @@ const express = require('express');
 const server = express();
 
 // your code here
+const dbaccount = require('./data/accounts-model.js');
+
+  server.get('/', (req, res) => {
+    res.send(`
+      <h2>DB TEST</h2>
+    `)
+  })
+
+  server.get('/api/accounts', (req, res) =>{
+    dbaccount.find()
+      .then( accounts => {
+        res.status(200).json(accounts)
+      })
+      .catch( err => {
+        res.status(500).json({ error: err, message:"Could not update data"})
+      })
+  })
+
+  server.get("/api/accounts/:id", (req, res) =>{
+    const id = req.params.id
+    dbaccount.findById(id)
+      .then( projectaccounts => {
+        res.status(200).json(projectaccounts)
+      })
+      .catch( err => {
+        res.status(500).json({ error: err, message:"Could not update data"})
+      })
+  })
 
 module.exports = server;
