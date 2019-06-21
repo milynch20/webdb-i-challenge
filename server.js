0d239280aa207e31280b72a147fb8d6de9f2d046
Malcolm Lynch <milynch1320@gmail.com>
2019-06-20 19:31:18 -0500

    mvp complete

--- server.js
+++ server.js
@@ -32,4 +32,47 @@ const dbaccount = require('./data/accounts-model.js');
       })
   })
 
+  server.post('/api/accounts', (req, res) => {
+    const account = req.body
+    dbaccount.add(account)
+      .then(account => {
+        res.status(200).json(account)
+      })
+      .catch(err => {
+        res.status(500).json({error: err, message: "Could not update data"})
+      })
+    })
+
+  server.put('/api/accounts/:id', (req, res) => {
+    const updateAccounts = req.body
+    const id = req.params.id
+
+    dbaccount.update(id, updateAccounts)
+      .then( project => {
+        res.status(200).json(project)
+      })
+      .catch( err => {
+        res.status(500).json({ error: err, message:"Could not update data"})
+      })
+  })
+
+  server.delete('/api/accounts/:id', (req, res)=>{
+    const accountsid = req.params.id
+    dbaccount.remove(accountsid)
+      .then( account =>{
+        if(account){
+          dbaccount.remove(account).then(
+            removeaccount => {
+              res.status(201).json(removeaccount)
+            }
+          )
+        }else{
+          res.status(404).json({ error: err, mesage : "User does not exist"})
+        }
+      })
+      .catch( err => {
+        res.status(500).json({ error: err, message:"Could not update data"})
+      })
+  })
+
 module.exports = server;
